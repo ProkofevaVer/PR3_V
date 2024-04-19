28166dc520a87480dffdaa749848fda23d7b021a
student <llallallall@bk.ru>
2024-04-19 15:44:00 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR3_Ver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввести число A - 0 \nВвести число B - 1\nВыполнить сложение - 3\nВыполнить вычитание - 4\n" +
+                "Выполнить умножение - 5\nВыполнить деление - 6");
+
+            Console.ReadKey();
+           //double A = Convert.ToDouble(Console.ReadLine("Введите число А"));
+        }
+    }
+}
