68e683566d8a9d6eb0fdd322f48c1a6c0298d69b
Fail <failkozyrev005@gmail.com>
2024-04-24 09:05:47 +0400

    реализована ветка вводА

--- Program.cs
+++ Program.cs
@@ -13,8 +13,17 @@ namespace PR3_Ver
             Console.WriteLine("Ввести число A - 0 \nВвести число B - 1\nВыполнить сложение - 3\nВыполнить вычитание - 4\n" +
                 "Выполнить умножение - 5\nВыполнить деление - 6");
 
-            Console.ReadKey();
-           //double A = Convert.ToDouble(Console.ReadLine("Введите число А"));
+
+            int A = 0;
+
+            while (true) 
+            {
+                int sv = int.Parse(Console.ReadLine());
+                if (sv == 0) 
+                {
+                    A = int.Parse(Console.ReadLine());
+                }
+            }
         }
     }
 }
