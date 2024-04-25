ba64bca87ff42c9e54d055d44db660754fcfda90
Paul_Utkin <@DESKTOP-04V9OQ3>
2024-04-25 17:11:36 +0400

    реализован функционал ветки move

--- Program.cs
+++ Program.cs
@@ -11,10 +11,20 @@ namespace PR3_Ver
         static void Main(string[] args)
         {
             Console.WriteLine("Ввести число A - 0 \nВвести число B - 1\nВыполнить сложение - 3\nВыполнить вычитание - 4\n" +
-                "Выполнить умножение - 5\nВыполнить деление - 6");
+                              "Выполнить умножение - 5\nВыполнить деление - 6");
 
-            Console.ReadKey();
-           //double A = Convert.ToDouble(Console.ReadLine("Введите число А"));
+            int A = 0;
+            int B = 0;
+            int mov = 0;
+
+            while (true)
+            {
+                int sv = int.Parse(Console.ReadLine());
+                if (sv == 5)
+                {
+                    mov = A * B;
+                }
+            }
         }
     }
 }
